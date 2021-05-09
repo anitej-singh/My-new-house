e7b36c96e78d7f7e4efed79aa2245938d765d94c
anitej-singh <83867119+anitej-singh@users.noreply.github.com>
2021-05-09 15:07:33 +0530

    Add files via upload

---
+++ My first house/main.js
@@ -0,0 +1,3 @@
+function showhouse() {
+    document.getElementById("img1").src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Tiny_house%2C_Portland.jpg/220px-Tiny_house%2C_Portland.jpg";
+}
